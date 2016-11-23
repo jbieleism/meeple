605ae820007a4d6bcc5fbd390c7033725b2cb32e
Justin Biele <justin.biele@gmail.com>
2016-11-23 00:04:33 -0800

    [add] Add backend chatroom controller

--- public/home/chatroom-controller.js
+++
@@ -1,9 +0,0 @@
-// const Chatroom = require('../models/chatroom');
-
-// module.exports.createChatroom = () => {
-
-//   let chatroom = new Chatroom(req.body);
-//   chatroom.save();
-//   res.json(req.body)
-
-// }
