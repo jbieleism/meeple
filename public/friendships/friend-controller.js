94bf753d5ad14d0df59b7c2128e6856a6e7b7f88
Justin Biele <justin.biele@gmail.com>
2016-11-26 02:13:46 -0800

    [add] Add user data to dom

--- public/friendships/friend-controller.js
+++ public/friendships/friend-controller.js
@@ -3,7 +3,14 @@
 
     $http.get('/api/friends/get')
       .success(function(response){
-        console.log(response)
+        console.log(response);
+        $scope.users = response
       })
+      .error(function(error){
+        console.log("There has been a grave error: ", error)
+      })
+
+
+
 
   }]);
