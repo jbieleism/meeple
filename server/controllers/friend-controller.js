97e89506b622c15234c201f0f7bb042abb493bf8
Justin Biele <justin.biele@gmail.com>
2016-11-26 02:02:53 -0800

    [add] Add results from db query

--- server/controllers/friend-controller.js
+++ server/controllers/friend-controller.js
@@ -3,7 +3,17 @@ const User = require ('../models/user');
 
 module.exports.getUsers = (req, res) => {
 
-  console.log("Im here in the get users function")
+  let user = new User(req.body);
+  User.find({}, (err, results) => {
+    if (err){
+      res.send((err) => {
+        console.log("There was an error: ", err)
+      })
+    }
+    else{
+      console.log("These are the results: ", results)
+    }
+  })
 
 }
 
